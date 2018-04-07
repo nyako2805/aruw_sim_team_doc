9844a1d5ed2ba93fd922e0ea0fa76eab7fcc39aa
nyako2805 <nyako2850@gmail.com>
2018-04-06 21:17:43 -0700

    Create sidenav.ts

---
+++ sidenav.ts
@@ -0,0 +1,64 @@
+// JavaScript source code
+
+document.write('\
+\
+    <div class="side-navigation">\
+        <a href="" class="category">Bullet</a>\
+        <section class="links">\
+            <a href="ARUW_BulletLife.html"> BulletLife </a>\
+        </section>\
+        <a href="" class="category">Camera</a>\
+        <section class="links" >\
+            <a href="ARUW_CameraControl.html"> CameraControl </a>\
+        </section>\
+        <a href="" class="category">Field</a>\
+        <section class="links" >\
+            <a href="ARUW_CoolingBuffZone.html"> CoolingBuffZone </a>\
+            <a href="ARUW_DefenseBuffZone.html"> DefenseBuffZone </a>\
+            <a href="ARUW_Refill17MMZone.html"> Refill17MMZone </a>\
+            <a href="ARUW_Refill42MMZone.html"> Refill42MMZone </a>\
+            <a href="ARUW_Regeneration.html"> Regeneration </a>\
+        </section>\
+        <a href="" class="category">Managers</a>\
+        <section class="links">\
+            <a href="ARUW_GameManager.html"> GameManager </a>\
+            <a href="ARUW_TankManager.html"> TankManager </a>\
+        </section>\
+        <a href="" class="category">Menu</a>\
+        <section class="links">\
+            <a href="ARUW_HostSettings.html"> HostSettings </a>\
+            <a href="ARUW_MainMenu.html"> MainMenu </a>\
+        </section>\
+        <a href="" class="category">Other</a>\
+        <section class="links">\
+            <a href="ARUW_BaseBehavior.html"> BaseBehavior </a>\
+            <a href="ARUW_CustomNetworkManager.html"> CustomNetworkManager </a>\
+            <a href="ARUW_DynamicCamera.html"> DynamicCamera </a>\
+            <a href="ARUW_Player.html"> Player </a>\
+            <a href="ARUW_PlayerEntry.html"> PlayerEntry </a>\
+            <a href="ARUW_Store.html"> Store </a>\
+            <a href="ARUW_SyncablePlayer.html"> SyncablePlayer </a>\
+            <a href="ARUW_Utils.html"> Utils </a>\
+        </section>\
+        <a href="" class="category">Tank</a>\
+        <section class="links">\
+            <a href="ARUW_TankHealth.html"> TankHealth </a>\
+            <a href="ARUW_TankMovement.html"> TankMovement </a>\
+            <a href="ARUW_TankNetworking.html"> TankNetworking </a>\
+            <a href="ARUW_TankShooting.html"> TankShooting </a>\
+            <a href="ARUW_TankXP.html"> TankXP </a>\
+        </section>\
+        <a href="" class="category">TargetPlate</a>\
+        <section class="links">\
+            <a href="ARUW_RobotDamage.html"> RobotDamage </a>\
+        </section>\
+        <a href="" class="category"> UI </a>\
+        <section class="links">\
+            <a href="ARUW_Billboard.html"> Billboard </a>\
+            <a href="ARUW_PlayerHealthBar.html"> PlayerHealthBar </a>\
+            <a href="ARUW_TestingUI.html"> TestingUI </a>\
+            <a href="ARUW_UIDirectionControl.html"> UIDirectionControl </a>\
+        </section>\
+     </div>\
+\
+');
