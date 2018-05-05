658e30f1a00999d530c3de294d7851aec5f7aeec
nyako2805 <nyako2850@gmail.com>
2018-05-04 19:16:08 -0700

    Add files via upload

---
+++ navbar.ts
@@ -0,0 +1,23 @@
+"use strict";
+(function () {
+
+    window.onload = function () {
+        navBar();
+    };
+    
+    function navBar() {
+        let a = document.createElement("a");
+        a.classList.add("category");
+        a.innerHTML = "Bullet";
+        a.href = "";
+        document.getElementsByClassName("side-navigation")[0].appendChild(a);
+        let b = document.createElement("section");
+        b.classList.add("links");
+        let c = document.createElement("a");
+        c.href = "ARUW_BulletLife.html";
+        c.innerHTML = "BulletLife";
+        b.appendChild(c);
+        document.getElementsByClassName("side-navigation")[0].appendChild(b);
+    }
+
+})();
